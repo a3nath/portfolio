a38c271c907abcbd29860af41bf4298a79c38424
a3nath <amarn.sks@gmail.com>
2021-08-17 22:38:05 -0400

    star start

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-const canvas = document.querySelector('.canvas');
+const canvas = document.querySelector('.mycanvas');
 const ctx = canvas.getContext('2d'); 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+ctx.beginPath();
+//starting point
+ctx.moveTo(10,90);
+ctx.quadraticCurveTo(60,10,90,90)
+ctx.lineTo(60,10)
+ctx.stroke()
+ctx.closePath()
+
+
+
+
+
+
+ctx.stroke()
 
 let isDrawing = false;
 
