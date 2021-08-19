d913a4a004af76b76ba8761e8908aa928f0e472a
a3nath <amarn.sks@gmail.com>
2021-08-18 23:40:16 -0400

    dropping star

--- script.js
+++ script.js
@@ -12,10 +12,26 @@ canvas.height = window.innerHeight;
 // ctx.closePath()
 
 let currentAngle = -0.5 * Math.PI
+let radius = 15, xCenter = 100 + radius, yCenter = 300 + radius;
+let speedX = 100, speedY = 60
+let lastTime = null;
+
+function frame(time){
+    if (lastTime != null) {
+        updateAnimation(Math.min(100, time - lastTime)/ 1000);
+    }
+    lastTime = time;
+    requestAnimationFrame(frame)
+}
+
+requestAnimationFrame(frame)
 
 
-function star(x, y) {
-    let radius = 50, xCenter = x + radius, yCenter = y + radius;
+function updateAnimation(step){
+    xCenter += step * speedX;
+    yCenter += step * speedY;
+    if (xCenter < radius || xCenter > canvas.width - radius) speedX = -speedX;
+    if (yCenter < radius || yCenter > canvas.width - radius) speedY = -speedY;
     ctx.beginPath();
     ctx.moveTo(xCenter + radius, yCenter);
     for (let i = 1; i <= 8; i++) {
@@ -26,10 +42,37 @@ function star(x, y) {
     }
     ctx.fillStyle = "gold";
     ctx.fill();
-  }
-  star(440, 20);
+}
 
-let isDrawing = false;
+
+
+// for (let width = 0;  width <= canvas.width; width+= 10) {
+//     star(width,Math.random() * canvas.height )
+// }
+
+// function play(){
+//     let scale = 0.5
+//        for (let width = 0;  width <= canvas.width; width+= 10) {
+//     star(width * scale,Math.random() * canvas.height )
+//     }
+        
+// }
+
+// function animate(){
+//     play()
+//     requestAnimationFrame(animate)
+// }
+
+// animate()
+
+star(Math.random() * canvas.width ,Math.random() * canvas.height)
+
+
+//rotate the stars
+//animation
+
+  
+
 
 function draw(e){
     if (!isDraw) return;
